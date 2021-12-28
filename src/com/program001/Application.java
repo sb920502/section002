9ca64b35bc971d8372a96198a15f6713bb0b87ab
sb920502 <bhowmik920502@gmail.com>
2021-12-28 17:40:48 +0600

    program001 program002

--- src/com/program001/Application.java
+++ src/com/program001/Application.java
@@ -2,6 +2,10 @@ package com.program001;
 
 class Application {
     public static void main(String[] args) {
-        
+        Runner runner1 = new Runner();
+        runner1.start();
+
+        Runner runner2 = new Runner();
+        runner2.start();
     }
 }
--- src/com/program002/Application.java
+++ src/com/program002/Application.java
@@ -2,6 +2,9 @@ package com.program002;
 
 class Application {
     public static void main(String[] args) {
-        
+        Thread t1 = new Thread(new Runner());
+        Thread t2 = new Thread(new Runner());
+        t1.start();
+        t2.start();
     }
 }
